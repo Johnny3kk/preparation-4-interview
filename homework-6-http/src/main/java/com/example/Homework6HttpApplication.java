104afabd1c40b0c177cf4d7028a2509c386dbd45
Johnny3kk <some@mail.ru>
2021-02-19 13:36:03 +0300

    HomeWork6 - 2 - simple http (get/post) client for Spring Boot

---
+++ homework-6-http/src/main/java/com/example/Homework6HttpApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework6HttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework6HttpApplication.class, args);
+    }
+
+}
